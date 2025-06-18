30fdce71d8799cdca74afbe1a522138ee14253fa
aiinsyafiqah <nurainsyafiqahnorazlan@gmail.com>
2025-06-18 17:51:33 +0800

    initialize commit

---
+++ lab6_q2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Lab 6 Q2</title>
+    </head>
+
+    <body>
+        <?php 
+        $students = [
+            [
+                'name' => 'Alice',
+                'program' => 'BIP',
+                'age' => 21
+            ],
+            [
+                'name' => 'Bob',
+                'program' => 'BIS',
+                'age' => 20
+            ],
+            [
+                'name' => 'Raju',
+                'program' => 'BIT',
+                'age' => 22
+            ]
+            ];
+
+        foreach ($students as $key => $value)
+            echo $key . ": " . $value . "<br"
+        ?>
+
+        
+
+    </body>
