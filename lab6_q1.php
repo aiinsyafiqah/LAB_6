64be1cd64da8e9bccd57ae4310a298fc8669cb34
aiinsyafiqah <nurainsyafiqahnorazlan@gmail.com>
2025-06-19 12:48:25 +0800

    change into my own infomation

--- lab6_q1.php
+++ lab6_q1.php
@@ -7,12 +7,36 @@
 <body>
 <?php
 $name = "Muhammad Asyraf";
+$matric = "CI230153";
+$course = "Information Technology (BIT)";
+$yearStudy = 3;
+$address = "No.598, Jalan S2 H17, Park Avenue, Seremban, 70300, Negeri Sembilan";
 ?>
 
-<table>
+<table border="1">
 <tr>
 <td>Name</td>
 <td><?php echo "$name"; ?></td>
+</tr>
+
+<tr>
+<td>Matric Number</td>
+<td><?php echo "$matric"; ?></td>
+</tr>
+
+<tr>
+<td>Course</td>
+<td><?php echo "$course"; ?></td>
+</tr>
+
+<tr>
+<td>Year of Study</td>
+<td><?php echo "$yearStudy"; ?></td>
+</tr>
+
+<tr>
+<td>Address</td>
+<td><?php echo "$address"; ?></td>
 </tr>
 
     </table>
